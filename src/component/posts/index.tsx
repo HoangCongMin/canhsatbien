f39a5abeb58f348dbb8e11b42e3f9a9e77a3dd97
hoang cong minh <0782708546hoangcongminh@gmail.com>
2023-04-28 01:02:43 +0700

    ok fix ok

--- src/component/posts/index.tsx
+++ src/component/posts/index.tsx
@@ -7,10 +7,10 @@ export default function posts() {
       <h1 className='text-[27px] font-bold hover:text-[#191970]'>
         Phó Chủ tịch Quốc hội Trần Quang Phương thăm và làm việc với Bộ Tư lệnh Cảnh sát biển
       </h1>
-      <div className='overflow-hidden cursor-pointer relative group'>
+      <div className='overflow-hidden cursor-pointer relative group mt-2'>
         <img src={Manage} alt='' className='w-full object-cover  group-hover:scale-110 transition duration-300 ease-in-out hover:brightness-50' />
       </div>
-      <div className='text-sm text-[#3b4e68]'>
+      <div className='text-sm text-[#3b4e68] mt-3'>
         (Chinhphu.vn) - Sáng 25/4 tại Trụ sở Chính phủ, Thủ tướng Phạm Minh Chính chủ trì cuộc họp Thường trực Chính phủ
         với các bộ
       </div>
